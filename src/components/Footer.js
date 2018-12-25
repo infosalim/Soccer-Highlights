057f9548d63d8e2cb8cbf6f015cd60158e2c6981
Salim Hossain <salimh5757@gmail.com>
2018-12-26 02:40:30 +0600

    created header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <header>
-            <div>Footer</div>
-        </header>
-    );
-}
+  return (
+    <header>
+      <div className="flexbox-container">
+        <Link to="/" className="logo">
+          <span />
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Footer;
